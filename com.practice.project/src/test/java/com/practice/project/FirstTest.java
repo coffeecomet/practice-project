ce5a6fb11ac0ce0772d94d2198384058d788c4b0
coffeecomet <hpates@gmail.com>
2015-10-22 15:15:10 +0800

    Adding testChrome function.

--- com.practice.project/src/test/java/com/practice/project/FirstTest.java
+++ com.practice.project/src/test/java/com/practice/project/FirstTest.java
@@ -1,15 +1,25 @@
 package com.practice.project;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class FirstTest {
 	
-	@Test
+	@Test(enabled=false)
 	public void testFirefox(){
 		
 		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("http://www.facebook.com");
+		driver.quit();
+	}
+	
+	@Test
+	public void testChrome(){
+		
+		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("http://www.facebook.com");
 		driver.quit();
