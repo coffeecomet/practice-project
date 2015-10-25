7a0d091c94821a55b50e1bdf01df6558a1d82a0b
coffeecomet <hpates@gmail.com>
2015-10-25 22:16:03 +0800

    Modified FirstTest.java; Moved testng.xml; Corrected typo error in pom.xml

--- com.practice.project/src/test/java/com/practice/project/FirstTest.java
+++ com.practice.project/src/test/java/com/practice/project/FirstTest.java
@@ -1,27 +1,73 @@
 package com.practice.project;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import junit.framework.Assert;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class FirstTest {
 	
-	@Test(enabled=false)
-	public void testFirefox(){
+	WebDriver driver = null;
+	private StringBuffer verificationErrors = new StringBuffer();
+	
+	@Parameters({"platform", "browser", "version", "url"})
+	@BeforeTest(alwaysRun=true)
+	public void setup(String platform, String browser, String version, String url) throws MalformedURLException {
 		
-		WebDriver driver = new FirefoxDriver();
-		driver.manage().window().maximize();
-		driver.get("http://www.facebook.com");
-		driver.quit();
+		DesiredCapabilities caps = new DesiredCapabilities();
+		
+		//Platform
+		if(platform.equalsIgnoreCase("Windows"))
+			caps.setPlatform(org.openqa.selenium.Platform.WINDOWS);
+		
+		//Browser
+		if(browser.equalsIgnoreCase("Firefox"))
+			caps = DesiredCapabilities.firefox();
+		
+		//Version
+		caps.setVersion(version);
+		
+		driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), caps);
+		
+		driver.get(url);
+		
 	}
 	
-	@Test
-	public void testChrome(){
+	@Test(description="Test Sample Form")
+	public void testSampleForm() throws InterruptedException {
 		
-		WebDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.get("http://www.facebook.com");
+		WebElement name = driver.findElement(By.id("entry_785445797"));
+		name.sendKeys("Halley");
+		
+		try{
+			
+			WebElement inputName = driver.findElement(By.id("entry_785445797"));
+			Assert.assertEquals(inputName.getAttribute("value"),"Halley");
+			
+		}catch(Error e){
+			verificationErrors.append(e.toString());
+		}
+	}
+	
+	@AfterTest
+	public void afterTest(){
+		
 		driver.quit();
+		
+		String verificationErrorString = verificationErrors.toString();
+		
+		if(!"".equals(verificationErrorString)){
+			Assert.fail(verificationErrorString);
+		}
 	}
 }
